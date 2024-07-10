fc3fa5d09693bf048e6997723f7ed1304f4b9b60
Hansraj61 <95349316+Hansraj61@users.noreply.github.com>
2024-07-10 14:56:47 +0530

    Add files via upload

---
+++ email.py
@@ -0,0 +1,33 @@
+import pickle 
+import streamlit as st
+import pandas as pd
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+model = pickle.load(open(r"C:\Users\dell\model.pkl","rb"))
+with open(r"C:\Users\dell\model1.pkl","rb") as f:
+    bow = pickle.load(f)
+          
+st.header("EMAIL SPAM OR HAM")
+#st.title("Email Spam/Ham Classifier")
+
+# Input email text
+Email = st.text_input("Paste the email here:")
+
+# Check if the email input is not empty
+if Email:
+    # Transform the input email text to feature array
+    data = bow.transform([Email]).toarray()
+
+    # Predict if the email is spam or ham
+    spam_ham = model.predict(data)[0]
+
+    # Display the prediction when the button is pressed
+    if st.button('Submit'):
+        if spam_ham == "spam":
+            st.write("The email is:",  spam_ham)
+            st.image(r"C:\Users\dell\Desktop\spam-mail-19097451.webp",width=200)
+        else:
+            st.write("The email is:",  spam_ham)
+
