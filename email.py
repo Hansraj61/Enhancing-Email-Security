f5e4298dfc0cf29d73f831e5685f93b0780e5277
Hansraj61 <95349316+Hansraj61@users.noreply.github.com>
2024-07-10 16:02:02 +0530

    email.py

--- email.py
+++ email.py
@@ -8,7 +8,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 model = pickle.load(open("model.pkl","rb"))
 with open("model1.pkl","rb") as f:
     bow = pickle.load(f)
-          
+st.image("innomatics.jpg")
+
+
 st.header("EMAIL SPAM OR HAM")
 #st.title("Email Spam/Ham Classifier")
 
